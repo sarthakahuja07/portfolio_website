558d64c57a152b6fbe7c458b53a83ab71fe40ce9
sarthak <sarthak.ahuja0007@gmail.com>
2022-10-03 19:35:02 +0530

    hosting

--- components/About.tsx
+++ components/About.tsx
@@ -8,7 +8,7 @@ function About({}: Props): ReactElement {
 		<div className="container mx-auto grid grid-cols-4 md:grid-cols-12 gap-x-5 mt-[92px] sm:mt[40px] md:mt[130px] lg:mt[158px] xl:mt-[234px]">
 			<div className="col-start-1 col-span-4 md:col-span-12 lg:col-start-2 lg:col-span-10 xl:col-start-1 xl:col-span-11 xxl:col-start-2 xxl:col-span-10 bg-active flex flex-col xl:flex-row justify-between ">
 				<div className="sm:w-[568px] md:w-[580px] xl:w-[520px] xxl:w-[580px] bg-red">
-					About
+					About me
 				</div>
 				<img
 					src="images/hero-bg.png"
