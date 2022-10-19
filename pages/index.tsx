42bfafddbf48eaaa1a7d1aa806c22ff79ba0efd4
sarthak <sarthak.ahuja0007@gmail.com>
2022-10-20 01:45:36 +0530

    star load

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,6 +40,16 @@ const Home: NextPage<Props> = ({ skillSet, experiences, projects }) => {
 			})
 		})
 		await Promise.all(promises)
+
+		// load the star.png image
+		const promise = new Promise((resolve, reject) => {
+			const img = new Image()
+			img.src = "/images/star.png"
+			img.onload = resolve
+			img.onerror = reject
+		})
+		await Promise.all([promise])
+
 		setLoading(false)
 	}
 
