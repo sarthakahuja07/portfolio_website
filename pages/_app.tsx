6c25516730d8910769bc53340891ef0a3125be6c
sarthak <sarthak.ahuja0007@gmail.com>
2022-10-20 17:38:32 +0530

    loading page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,89 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
+import { useEffect, useState } from "react"
+import backgrounds from "../public/backgrounds"
+import { motion } from "framer-motion"
+
+const icon = {
+	hidden: {
+		opacity: 0,
+		pathLength: 0,
+		fill: "rgba(0, 0, 0, 0)"
+	},
+	visible: {
+		opacity: 1,
+		pathLength: 1,
+		fill: "rgba(0, 0, 0, 1)"
+	}
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+	const [loading, setLoading] = useState(true)
+
+	const cacheImages = async () => {
+		const promises = await backgrounds.map((image) => {
+			return new Promise((resolve, reject) => {
+				const img = new Image()
+				img.src = image.url
+				img.onload = resolve
+				img.onerror = reject
+			})
+		})
+
+		promises.push(
+			new Promise((resolve, reject) => {
+				setTimeout(resolve, 2000)
+			})
+		)
+
+		await Promise.all(promises)
+
+		setLoading(false)
+	}
+
+	useEffect(() => {
+		cacheImages()
+	}, [])
+
+	return (
+		<>
+			{loading ? (
+				<div className="flex justify-center items-center h-screen bg-modal">
+					<div className="w-[250px] h-[250px] drop-shadow-loader flex items-center justify-center  bg-[#000] rounded-[30px]">
+						<motion.svg
+							width="402"
+							height="402"
+							viewBox="0 0 402 402"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+							className="w-[56%] overflow-visible stroke-active stroke-[10px]"
+						>
+							<motion.path
+								d="M399.431 101.68V177.871L209.627 85.3537H190.804L85.7059 141.136C74.4118 166.17 97.732 169.707 110.804 168.347L201.784 122.088L401 221.408V301.68L201.784 401L2.56863 301.68V222.769L192.373 320.728H209.627L316.294 263.585C324.137 240.456 306.882 233.653 289.627 237.735L201.784 281.272L1 181.952V98.9592L201.784 1L399.431 101.68Z"
+								variants={icon}
+								initial="hidden"
+								animate="visible"
+								transition={{
+									default: {
+										duration: 2,
+										ease: "easeOut",
+										repeat: Infinity
+									},
+									fill: {
+										duration: 2,
+										ease: "easeOut",
+										repeat: Infinity
+									}
+								}}
+							/>
+						</motion.svg>
+					</div>
+				</div>
+			) : (
+				<Component {...pageProps} />
+			)}
+		</>
+	)
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next"
 import Head from "next/head"
 import Navbar from "../components/Navbar"
-import styles from "../styles/Home.module.css"
 import { Footer } from "../components/Footer"
 import Contact from "../components/Contact"
 import Hero from "../components/Hero"
@@ -16,9 +15,7 @@ import {
 	projects
 } from "@prisma/client"
 import backgrounds from "../public/backgrounds"
-import { useEffect, useState } from "react"
-import { url } from "inspector"
-import { motion } from "framer-motion"
+import { useEffect } from "react"
 
 type SkillsSetType = skills_set & { skills: skills[] }
 
@@ -28,47 +25,7 @@ interface Props {
 	projects: projects[]
 }
 
-const icon = {
-	hidden: {
-		opacity: 0,
-		pathLength: 0,
-		fill: "rgba(0, 0, 0, 0)"
-	},
-	visible: {
-		opacity: 1,
-		pathLength: 1,
-		fill: "rgba(0, 0, 0, 1)"
-	}
-}
-
 const Home: NextPage<Props> = ({ skillSet, experiences, projects }) => {
-	const [loading, setLoading] = useState(true)
-
-	const cacheImages = async () => {
-		const promises = await backgrounds.map((image) => {
-			return new Promise((resolve, reject) => {
-				const img = new Image()
-				img.src = image.url
-				img.onload = resolve
-				img.onerror = reject
-			})
-		})
-
-		promises.push(
-			new Promise((resolve, reject) => {
-				setTimeout(resolve, 2000)
-			})
-		)
-
-		await Promise.all(promises)
-
-		setLoading(false)
-	}
-
-	useEffect(() => {
-		cacheImages()
-	}, [])
-
 	const randomBackground = () => {
 		const randomindex = Math.floor(Math.random() * backgrounds.length)
 		return backgrounds[randomindex].url
@@ -77,12 +34,10 @@ const Home: NextPage<Props> = ({ skillSet, experiences, projects }) => {
 	useEffect(() => {
 		const background = randomBackground()
 		console.log(background)
-		if (!loading) {
-			document.getElementById(
-				"bg"
-			)!.style.backgroundImage = `url(${background})`
-		}
-	}, [loading])
+		document.getElementById(
+			"bg"
+		)!.style.backgroundImage = `url(${background})`
+	}, [])
 
 	return (
 		<div>
@@ -95,60 +50,24 @@ const Home: NextPage<Props> = ({ skillSet, experiences, projects }) => {
 				<link rel="icon" href="/favicon.svg" />
 			</Head>
 
-			{loading ? (
-				<div className="flex justify-center items-center h-screen bg-modal">
-					<div className="w-[250px] h-[250px] drop-shadow-loader flex items-center justify-center  bg-[#000] rounded-[30px]">
-						<motion.svg
-							width="402"
-							height="402"
-							viewBox="0 0 402 402"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-							className="w-[56%] overflow-visible stroke-active stroke-[10px]"
-						>
-							<motion.path
-								d="M399.431 101.68V177.871L209.627 85.3537H190.804L85.7059 141.136C74.4118 166.17 97.732 169.707 110.804 168.347L201.784 122.088L401 221.408V301.68L201.784 401L2.56863 301.68V222.769L192.373 320.728H209.627L316.294 263.585C324.137 240.456 306.882 233.653 289.627 237.735L201.784 281.272L1 181.952V98.9592L201.784 1L399.431 101.68Z"
-								// fill="#09F755"
-								// stroke="#09F755"
-								variants={icon}
-								initial="hidden"
-								animate="visible"
-								transition={{
-									default: {
-										duration: 2,
-										ease: "easeOut",
-										repeat: Infinity
-									},
-									fill: {
-										duration: 2,
-										ease: "easeOut",
-										repeat: Infinity
-									}
-								}}
-							/>
-						</motion.svg>
-					</div>
+			<>
+				<Navbar />
+
+				<div
+					id="bg"
+					className=" bg-center bg-cover bg-no-repeat h-screen w-[100%] absolute inset-0 z-[-10]"
+				>
+					<div className="backdrop-blur-[100px] h-[250%]"></div>
 				</div>
-			) : (
-				<>
-					<Navbar />
+				<div className="mt-[66.5px] sm:mt-[109px] md:mt-[105px] lg:mt-[107px] xl:mt-[124px]"></div>
 
-					<div
-						id="bg"
-						className=" bg-center bg-cover bg-no-repeat h-screen w-[100%] absolute inset-0 z-[-10]"
-					>
-						<div className="backdrop-blur-[100px] h-[250%]"></div>
-					</div>
-					<div className="mt-[66.5px] sm:mt-[109px] md:mt-[105px] lg:mt-[107px] xl:mt-[124px]"></div>
-
-					<Hero />
-					<About experiences={experiences} />
-					<Skills skillSet={skillSet} />
-					<Work projects={projects} />
-					<Contact />
-					<Footer />
-				</>
-			)}
+				<Hero />
+				<About experiences={experiences} />
+				<Skills skillSet={skillSet} />
+				<Work projects={projects} />
+				<Contact />
+				<Footer />
+			</>
 		</div>
 	)
 }
