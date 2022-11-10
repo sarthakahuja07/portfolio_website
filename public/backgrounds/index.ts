3ea599dacdd827031e3e2885f6065a9ac7aac1d7
sarthak <sarthak.ahuja0007@gmail.com>
2022-11-10 17:49:34 +0530

    small background images

--- public/backgrounds/index.ts
+++ public/backgrounds/index.ts
@@ -1,56 +1,52 @@
 const backgrounds = [
 	{
 		name: "background1",
-		url: "https://i.imgur.com/qGSZGiB.png"
+		url: "https://i.imgur.com/27gIeVK.jpg"
 	},
 	{
 		name: "background1",
-		url: "https://i.imgur.com/CRe77nk.png"
+		url: "https://i.imgur.com/jGCkbrh.jpg"
 	},
 	{
 		name: "background1",
-		url: "https://i.imgur.com/o1a0gYK.png"
+		url: "https://i.imgur.com/6dhVJQY.jpg"
 	},
 	{
 		name: "background1",
-		url: "https://i.imgur.com/SNbyVHQ.png"
+		url: "https://i.imgur.com/5ebDtMM.jpg"
 	},
 	{
 		name: "background1",
-		url: "https://i.imgur.com/bxIYNcN.png"
+		url: "https://i.imgur.com/lzyajuu.jpg"
 	},
 	{
 		name: "background1",
-		url: "https://i.imgur.com/KXG71K9.png"
+		url: "https://i.imgur.com/b5oiPXt.jpg"
 	},
 	{
 		name: "background1",
-		url: "https://i.imgur.com/vFFhoHe.png"
+		url: "https://i.imgur.com/jCOhZvE.jpg"
 	},
 	{
 		name: "background1",
-		url: "https://i.imgur.com/TZfguBy.png"
+		url: "https://i.imgur.com/KIyQact.jpg"
 	},
 	{
 		name: "background1",
-		url: "https://i.imgur.com/RKLKV9I.png"
+		url: "https://i.imgur.com/Vc6ChMZ.jpg"
 	},
 	{
 		name: "background1",
-		url: "https://i.imgur.com/7yzYHGs.png"
+		url: "https://i.imgur.com/qe4TyBA.jpg"
 	},
 	{
 		name: "background1",
-		url: "https://i.imgur.com/tfW2YBy.png"
+		url: "https://i.imgur.com/YsMh1EP.jpg"
 	},
 	{
 		name: "background1",
-		url: "https://i.imgur.com/5MrrRnM.png"
-	},
-	{
-		name: "background1",
-		url: "https://i.imgur.com/5B8Wo5H.png"
-	},
+		url: "https://i.imgur.com/qKNKdAa.jpg"
+	}
 ]
 
 export default backgrounds
