5ba8c8592b8c45a7d971d11d9c538eadd1a5112d
sarthak <sarthak.ahuja0007@gmail.com>
2022-11-10 18:54:44 +0530

    Image

--- components/WorkCard.tsx
+++ components/WorkCard.tsx
@@ -6,6 +6,7 @@ import Globe from "../public/images/globe-white.svg"
 import { projects } from "@prisma/client"
 import { motion } from "framer-motion"
 import bounceVariant from "../animation/bounceVariant"
+import Image from "next/image"
 
 interface Props {
 	open: (project: projects) => void
@@ -15,9 +16,7 @@ interface Props {
 
 const WorkCard = ({ open, project, index }: Props) => {
 	return (
-		<div
-			className="mb-4 lg:mb-9 xl:mb-0 xl:mr-5 "
-		>
+		<div className="mb-4 lg:mb-9 xl:mb-0 xl:mr-5 ">
 			<div className="relative  w-[320px] card sm:w-[488px] xxl:w-[564px] lg:w-[482px] mx-auto mb-[46px] sm:mb-[72px] lg:mb-[92px] hover:z-[2] ">
 				<div className=" absolute top-0 right-0 w-fit h-[86px] sm:h-[130px] xxl:h-[168px] lg:h-[109px] cardNumber  ">
 					<p className="font-clash text-number outlinedNumber sm:text-numberMd lg:text-numberLg text-black_bg">
@@ -29,11 +28,16 @@ const WorkCard = ({ open, project, index }: Props) => {
 					className="bg-work-card-gradient  bg-cover bg-clip-padding cursor-pointer cardBox backdrop-blur-[6.86799px] rounded-work-card-xs sm:rounded-work-card-sm lg:rounded-work-card-lg border-solid border-[#FFFFFF0D] xxl:border-[2px] border-[1.5px] relative top-[46px] sm:top-[72px] lg:top-[92px] p-3 sm:p-5  lg:p-[30px] lg:px-[24px] xxl:p-[26px]"
 				>
 					<div className="flex flex-row justify-between items-center">
-						<img
-							src={project.cover}
-							className="w-[116px] h-[123px] sm:w-[176px] lg:w-[174px] lg:h-[187px] xxl:w-[228px] xxl:h-[244px] sm:h-[190px] xxl:rounded-[24px] lg:rounded-[18.39px] rounded-[12px] sm:rounded-[18.5px]"
-							alt="project image"
-						/>
+						<div className="w-[116px] h-[123px] sm:w-[176px] lg:w-[174px] lg:h-[187px] xxl:w-[228px] xxl:h-[244px] sm:h-[190px] relative">
+							<Image
+								src={project.cover}
+								alt={project.name}
+								layout="fill"
+								className=" xxl:rounded-[24px] lg:rounded-[18.39px] rounded-[12px] sm:rounded-[18.5px] w-[116px] h-[123px] sm:w-[176px] lg:w-[174px] lg:h-[187px] xxl:w-[228px] xxl:h-[244px] sm:h-[190px]"
+							/>
+						</div>
+
+						
 						<div className="font-clash w-[156px] sm:w-[240px] lg:w-[244px]">
 							<h3 className="text-phoneXl sm:text-xsm lg:text-md mb-1 sm:mb-2">
 								{project.name}
