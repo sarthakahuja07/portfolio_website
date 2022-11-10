5849fe7aa622926fc8b4374bcdc1e69fb724ff20
sarthak <sarthak.ahuja0007@gmail.com>
2022-11-10 18:16:04 +0530

    webp

--- public/backgrounds/index.ts
+++ public/backgrounds/index.ts
@@ -1,52 +1,52 @@
 const backgrounds = [
 	{
 		name: "background1",
-		url: "https://i.imgur.com/27gIeVK.jpg"
+		url: "./images/background.webp"
 	},
-	{
-		name: "background1",
-		url: "https://i.imgur.com/jGCkbrh.jpg"
-	},
-	{
-		name: "background1",
-		url: "https://i.imgur.com/6dhVJQY.jpg"
-	},
-	{
-		name: "background1",
-		url: "https://i.imgur.com/5ebDtMM.jpg"
-	},
-	{
-		name: "background1",
-		url: "https://i.imgur.com/lzyajuu.jpg"
-	},
-	{
-		name: "background1",
-		url: "https://i.imgur.com/b5oiPXt.jpg"
-	},
-	{
-		name: "background1",
-		url: "https://i.imgur.com/jCOhZvE.jpg"
-	},
-	{
-		name: "background1",
-		url: "https://i.imgur.com/KIyQact.jpg"
-	},
-	{
-		name: "background1",
-		url: "https://i.imgur.com/Vc6ChMZ.jpg"
-	},
-	{
-		name: "background1",
-		url: "https://i.imgur.com/qe4TyBA.jpg"
-	},
-	{
-		name: "background1",
-		url: "https://i.imgur.com/YsMh1EP.jpg"
-	},
-	{
-		name: "background1",
-		url: "https://i.imgur.com/qKNKdAa.jpg"
-	}
+	// {
+	// 	name: "background1",
+	// 	url: "https://i.imgur.com/jGCkbrh.jpg"
+	// },
+	// {
+	// 	name: "background1",
+	// 	url: "https://i.imgur.com/6dhVJQY.jpg"
+	// },
+	// {
+	// 	name: "background1",
+	// 	url: "https://i.imgur.com/5ebDtMM.jpg"
+	// },
+	// {
+	// 	name: "background1",
+	// 	url: "https://i.imgur.com/lzyajuu.jpg"
+	// },
+	// {
+	// 	name: "background1",
+	// 	url: "https://i.imgur.com/b5oiPXt.jpg"
+	// },
+	// {
+	// 	name: "background1",
+	// 	url: "https://i.imgur.com/jCOhZvE.jpg"
+	// },
+	// {
+	// 	name: "background1",
+	// 	url: "https://i.imgur.com/KIyQact.jpg"
+	// },
+	// {
+	// 	name: "background1",
+	// 	url: "https://i.imgur.com/Vc6ChMZ.jpg"
+	// },
+	// {
+	// 	name: "background1",
+	// 	url: "https://i.imgur.com/qe4TyBA.jpg"
+	// },
+	// {
+	// 	name: "background1",
+	// 	url: "https://i.imgur.com/YsMh1EP.jpg"
+	// },
+	// {
+	// 	name: "background1",
+	// 	url: "https://i.imgur.com/qKNKdAa.jpg"
+	// }
 ]
 
 export default backgrounds
